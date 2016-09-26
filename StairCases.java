6ff3e45853edb39e83c380b8cf459a35bb8c7959
Dillon Shu <shudillon@gmail.com>
2016-09-26 08:58:33 -0400

    Functional ascending and descending printing function

--- StairCases.java
+++ StairCases.java
@@ -1,33 +1,82 @@
 /**
- * Write javadoc for your class here (and remove this comment, or course)
- *
+ * This class creates a staircase of stars, both ascending and descending.
+ *@author Dillon Shu
+ *@version 1.0
  */
-
+import java.util.Scanner;
 public class StairCases
 {
   /**
-   * Main method demonstrates the other wo methods by calling each of them.
+   * Main method demonstrates the other Two methods by calling each of them.
    * @param args Standard array of commandline String arguments. Not used in this program.
    */
 
-  public static void main(String[] args)
+public static void main(String[] args)
   {
-    //TODO change the next line so it actually gets input from the user
-    int size = 4;
-
+	Scanner user = new Scanner(System.in);
+	System.out.println("Hi! Please input an integer to see a cool staircase! ");
+	int size = user.nextInt();
+	System.out.println("Great, Thanks!");
+	System.out.println("");
+	
+	
     printAscending(size);
     printDescending(size);
   }
 
   /**
-   * Write your javadoc here
+   * Method organizes how the stars (from forLoop) are printed out in an ascending fashion
+   *@param takes in an integer that determines the size of the staircase
    */
-  public static void printAscending
-  //TODO Finish this method
+
+
+public static void printAscending(int size)
+{
+	int start = 0;
+	while(start < size)
+	{
+		int zero = 0;
+		forLoop(zero,start);
+		start ++;
+	}
+}
 
   /**
-   * Write your javadoc here
+   * Prints out the correct number of stars for printAscending
+   *@param takes in two ints, one that determines the starting number of stars (per line) and one that determines the ending number
    */
-  public static void printDescending
-  //TODO Finish this method
+public static void forLoop(int begin, int finish)
+{
+	for (int counter = begin; counter <= finish; counter++)
+	{
+		System.out.print("*");
+	}
+	System.out.println("");
+}
+  /**
+   *Prints out the correct number of stars for printDescending
+   *@param takes in two ints, one that determines the starting number of stars (per line) and one that determines the ending number
+   */
+public static void forLoop2(int begin)
+{
+	for (int counter = begin; counter > 0; counter--)
+	{
+		System.out.print("*");
+	}
+	System.out.println("");
+}
+ /**
+  *Method organizes how the stars (from forLoop2) are printed out in an descending fashion
+  *@param takes in an integer that determines the size of the staircase
+  */
+public static void printDescending(int size)
+{
+	int start = size;
+	while(start > 0)
+	{
+		forLoop2(start);
+		start--;
+	}
+} 
+
 }
